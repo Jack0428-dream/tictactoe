ffd740d421b9de8304a0393c2a346b5fd1afa44a
Jack0428-dream <192861219+Jack0428-dream@users.noreply.github.com>
2025-07-12 20:34:00 +0900

    make gameboard

---
+++ tictactoe.js
@@ -0,0 +1,23 @@
+function Gameboard() {
+    const grid = 3;
+    const gameboard =[];
+
+    for(let i = 0; i < grid; i++) {
+        gameboard[i] = [];
+        for (let j = 0; j < grid; j++) {
+            gameboard[i].push(null);
+        }
+    }
+
+    const getboard = () => gameboard;
+
+    // const indexChoice = (index, ) => {
+    //     if ( index == 1 ) {
+
+    //     }
+    // }
+}
+
+function indexChoice(index) {
+    
+}
